5510cf428da6e6350588a40ec9e00e71ab8deca8
AMAGHZAZ-Y <yassine.amaghzaz@outlook.com>
2022-11-23 14:19:05 +0100

    first

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom/client';
+import App from './app';
+import React from 'react';
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
+

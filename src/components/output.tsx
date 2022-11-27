448eab057dcd0a9cf2a4fe0422695a59829cbb40
AMAGHZAZ-Y <yassine.amaghzaz@outlook.com>
2022-11-27 22:19:54 +0100

    added tabs

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -1,4 +1,4 @@
-import { Textarea } from "@chakra-ui/react";
+import { TabList, Tabs, Textarea, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import React from "react";
 import { StepStore } from "../stepcore/stores";
 
@@ -6,7 +6,31 @@ const Output = () => {
 	const [Steps] = StepStore((state) => ([state.Steps]))
 
 	return (
-		<Textarea defaultValue="Hello" value={JSON.stringify(Steps)} />
+		<Tabs>
+			<TabList>
+				<Tab>
+					Output
+				</Tab>
+				<Tab>
+					Simulation
+				</Tab>
+				<Tab>
+					Visual Nodes
+				</Tab>
+			</TabList>
+			<TabPanels>
+				<TabPanel>
+					<div>CSV output goes here...</div>
+					<div>+ Button to download it</div>
+				</TabPanel>
+				<TabPanel>
+					<div>Simulation goes in here...</div>
+				</TabPanel>
+				<TabPanel>
+					<iframe src="https://giphy.com/embed/FbPsiH5HTH1Di" width="480" height="445" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/long-far-FbPsiH5HTH1Di">via GIPHY</a></p>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
 	)
 }
 export default Output
